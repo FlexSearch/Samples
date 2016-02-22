a16b8a2d14401b892e53804e1c5c28cb443fcf63
vladnega <negacevschi.vladimir@gmail.com>
2016-02-22 01:07:02 +0000

    add Creating Index sample

---
+++ src/Samples/CSharpClient/CreatingIndex.cs
@@ -0,0 +1,33 @@
+﻿using FlexSearch.Api.Api;
+using FlexSearch.Api.Constants;
+using FlexSearch.Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpClient
+{
+    public class CreatingIndex
+    {
+        public void Work()
+        {
+            var indicesApi = new IndicesApi("http://localhost:9800");
+
+            var index = new Index("contact")
+            {
+                Fields = new Field[]
+                {
+                    new Field("name", FieldType.Text),
+                    new Field("age", FieldType.Int)
+                }
+            };
+
+            var response = indicesApi.CreateIndex(index);
+            Debug.Assert(response.Error?.Message == null);
+            var indexName = response.Data.Id; // should be "contact"
+        }
+    }
+}
