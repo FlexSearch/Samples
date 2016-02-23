8eaa02f3d9a13c840eac2898a036edd675540710
vladnega <negacevschi.vladimir@gmail.com>
2016-02-23 14:33:48 +0000

    add Modifying Documents sample

---
+++ src/Samples/CSharpClient/ModifyingDocuments.cs
@@ -0,0 +1,36 @@
+﻿using FlexSearch.Api.Api;
+using FlexSearch.Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpClient
+{
+    public class ModifyingDocuments
+    {
+        public void DoWork()
+        {
+            var documentsApi = new DocumentsApi("http://localhost:9800");
+
+            SetupSampleDocument(documentsApi);
+
+            var correctedDocument = new Document("7", "contact");
+            // We need to supply *ALL* fields, even if we're not changing them
+            correctedDocument.Fields.Add("name", "Vladimir");
+            correctedDocument.Fields.Add("age", "26");
+
+            documentsApi.UpdateDocument(correctedDocument, "contact", "7");
+        }
+
+        private void SetupSampleDocument(DocumentsApi documentsApi)
+        {
+            var doc = new Document("7", "contact");
+            doc.Fields.Add("age", "26");
+            doc.Fields.Add("name", "Vladimir");
+
+            documentsApi.CreateDocument(doc, "contact");
+        }
+    }
+}
