e42022de9100a6df20fb146526d6fd937fddd031
vladnega <negacevschi.vladimir@gmail.com>
2016-02-23 16:11:13 +0000

    add Closing Index sample

--- src/Samples/CSharpClient/ClosingIndex.cs
+++ src/Samples/CSharpClient/ClosingIndex.cs
@@ -3,13 +3,13 @@ using System;
 
 namespace CSharpClient
 {
-    public class DeleteIndex
+    public class ClosingIndex
     {
         public void DoWork()
         {
             var indicesApi = new IndicesApi("http://localhost:9800");
 
-            var response = indicesApi.DeleteIndex("contact");
+            var response = indicesApi.UpdateStatus("contact", "offline");
 
             // This method doesn't return any significant data. It just reports any errors.
             if (response.Error?.Message != null)
--- src/Samples/CSharpClient/ModifyingDocuments.cs
+++ src/Samples/CSharpClient/ModifyingDocuments.cs
@@ -1,10 +1,6 @@
 ﻿using FlexSearch.Api.Api;
 using FlexSearch.Api.Model;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpClient
 {
