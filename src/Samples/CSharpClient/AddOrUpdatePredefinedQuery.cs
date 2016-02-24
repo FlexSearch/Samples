45899b7d0d7aada1d9cdbbb384e2267b05bdf8b4
vladnega <negacevschi.vladimir@gmail.com>
2016-02-24 16:54:49 +0000

    add the Create Or Update Predefined Query sample

---
+++ src/Samples/CSharpClient/AddOrUpdatePredefinedQuery.cs
@@ -0,0 +1,30 @@
+﻿using FlexSearch.Api.Api;
+using FlexSearch.Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpClient
+{
+    public class AddOrUpdatePredefinedQuery
+    {
+        public void DoWork()
+        {
+            var indicesApi = new IndicesApi("http://localhost:9800");
+
+            var updatedQuery =
+                new SearchQuery("country", "allof(agriproducts, 'wheat', 'corn', 'grapes') AND like(countryname, @countryName)")
+                {
+                    QueryName = "agriSearch"
+                };
+
+            var response = indicesApi.UpdateIndexPredefinedQuery(updatedQuery, "country");
+
+            // This method doesn't return any significant data. It just reports any errors.
+            if (response.Error?.Message != null)
+                Console.WriteLine(response.Error.Message);
+        }
+    }
+}
