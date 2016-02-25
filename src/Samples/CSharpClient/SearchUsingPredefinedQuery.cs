7e0f4d8510adde768411dde160b43393f109e875
vladnega <negacevschi.vladimir@gmail.com>
2016-02-25 14:47:12 +0000

    add the Search using predefined query sample

---
+++ src/Samples/CSharpClient/SearchUsingPredefinedQuery.cs
@@ -0,0 +1,59 @@
+﻿using FlexSearch.Api.Api;
+using FlexSearch.Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpClient
+{
+    public class SearchUsingPredefinedQuery
+    {
+        public void SearchWithoutVariables()
+        {
+            var searchApi = new SearchApi("http://localhost:9800");
+
+            var query = new SearchQuery()
+            {
+                IndexName = "country",
+                QueryName = "agrisearch"
+            };
+
+            var response = searchApi.PostSearch(query, "country");
+
+            InterpretResponse(response);
+        }
+
+        public void SearchWithVariables()
+        {
+            var searchApi = new SearchApi("http://localhost:9800");
+
+            var query = new SearchQuery()
+            {
+                IndexName = "country",
+                QueryName = "agrisearch"
+            };
+
+            query.Variables.Add("countryname", "romania");
+
+            var response = searchApi.PostSearch(query, "country");
+
+            InterpretResponse(response);
+        }
+
+        private void InterpretResponse(SearchResultsResponse response)
+        {
+            if (response.Error?.Message != null)
+            {
+                Console.WriteLine(response.Error.Message);
+            }
+            else
+            {
+                var records = response.Data.Documents;
+                var countryNameOfFirstRecord = records[0].Fields["countryname"];
+                var totalAvailableNumberOfRecords = response.Data.TotalAvailable;
+            }
+        }
+    }
+}
